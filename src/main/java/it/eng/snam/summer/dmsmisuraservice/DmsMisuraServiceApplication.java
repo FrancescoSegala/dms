3d3da434f4d02d6f42a48015b5384d967f64ba08
lazminervi <manlio.minervini@eng.it>
2021-07-13 11:08:58 +0200

    Add certificate

--- src/main/java/it/eng/snam/summer/dmsmisuraservice/DmsMisuraServiceApplication.java
+++ src/main/java/it/eng/snam/summer/dmsmisuraservice/DmsMisuraServiceApplication.java
@@ -1,5 +1,9 @@
 package it.eng.snam.summer.dmsmisuraservice;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +15,26 @@ import it.eng.snam.summer.dmsmisuraservice.service.dds.FakeDDSImpl;
 import it.eng.snam.summer.dmsmisuraservice.service.summer.FakeSummerImpl;
 import it.eng.snam.summer.dmsmisuraservice.service.summer.Summer;
 import it.eng.snam.summer.dmsmisuraservice.service.summer.SummerImpl;
+import it.eng.snam.summer.dmsmisuraservice.util.UtilityControlM;
 
 @SpringBootApplication
 public class DmsMisuraServiceApplication {
+	
+	@Value("${controlm-jks.password:default}")
+    private String password;
+    
+    @Value("${controlm-jks.truststore:default}")
+    private String trustStorePath;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DmsMisuraServiceApplication.class, args);
 	}
+	
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Rome"));
+        UtilityControlM.setSslProperties(password, trustStorePath);
+    }
 
     @Bean
     public DDS dds(@Value("${external.dds.fake:false}") boolean fake ){
